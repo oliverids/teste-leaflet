45cb71ae3af992e23aabf75162e2bdf10b4f2f84
Isabela Oliveira <oliver.isabela2303@gmail.com>
2022-12-07 20:24:40 -0300

    teste com universidades em um json

--- main.js
+++ main.js
@@ -1,23 +1,69 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const map = L.map('map').setView([-20.32, -40.33], 4);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 12,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-setupCounter(document.querySelector('#counter'))
+// const marker = L.marker([-20.32, -40.33]).addTo(map)
+//   .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+let popupContent;
+
+fetch('./pontos.json').then(r => r.json()).then(r => {
+
+  for (let i = 0; i < r.length; i++) {
+    popupContent = `<a href="#${r[i].nome}"><h2>${r[i].nome}</h2></a>`;
+
+    L.marker([r[i].lat, r[i].lon]).addTo(map).bindPopup(popupContent);
+
+
+    let uniSection = document.createElement('section');
+    uniSection.id = `#${r[i].nome}`;
+    uniSection.classList.add('secao-uni');
+    uniSection.innerHTML = `<h2>${r[i].nome}</h2>`;
+    document.getElementById('about').appendChild(uniSection);
+  }
+})
+
+// L.DomEvent.addListener(popupEvent, 'click', function(event){
+//   console.log(event.target)
+// });
+
+map.on('popupopen', function (e) {
+  var marker = e.popup._source._popup._content,
+    anchor = marker.substring(
+      marker.indexOf('"') + 1,
+      marker.lastIndexOf('">'));
+
+  document.querySelectorAll('.secao-uni').forEach(e => e.classList.remove('ativo'));
+  document.getElementById(anchor).classList.add('ativo');
+});
+
+// const circle = L.circle([51.508, -0.11], {
+//   color: 'red',
+//   fillColor: '#f03',
+//   fillOpacity: 0.5,
+//   radius: 500
+// }).addTo(map).bindPopup('I am a circle.');
+
+// const polygon = L.polygon([
+//   [51.509, -0.08],
+//   [51.503, -0.06],
+//   [51.51, -0.047]
+// ]).addTo(map).bindPopup('I am a polygon.');
+
+
+// const popup = L.popup()
+//   .setLatLng([51.513, -0.09])
+//   .setContent('I am a standalone popup.')
+//   .openOn(map);
+
+// function onMapClick(e) {
+//   popup
+//     .setLatLng(e.latlng)
+//     .setContent(`You clicked the map at ${e.latlng.toString()}`)
+//     .openOn(map);
+// }
+
+// map.on('click', onMapClick);
+
