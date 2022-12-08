fe22579b94d2af706bed1596c863433ac7d4e9d1
Isabela Oliveira <oliver.isabela2303@gmail.com>
2022-12-08 19:50:19 -0300

    mais umas coisinhas

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const universidades = await fetch('https://deno-api-fake.deno.dev/api/universida
 
 let popupContent;
 
+const secaoAbout = document.querySelector('#about .container');
 
 //UNIVERSIDADES FETCH
 let uniArray = [];
@@ -20,7 +21,7 @@ for (let i = 0; i < universidades.length; i++) {
         <h2>${universidades[i].nome}</h2>
         <p>Aqui terão informações sobre a ${universidades[i].nome}.</p>
       `;
-    document.getElementById('about').appendChild(uniSection);
+    secaoAbout.appendChild(uniSection);
 }
 const uniLayer = L.layerGroup([...uniArray]);
 
@@ -33,15 +34,19 @@ for (let i = 0; i < users.length; i++) {
     let marker = L.marker([users[i].address.geo.lat, users[i].address.geo.lng]).bindPopup(popupContent);
     userArray.push(marker);
 
-    // let uniSection = document.createElement('section');
-    // uniSection.id = `#${users[i].name}`;
-    // uniSection.classList.add('secao-uni');
-    // uniSection.innerHTML = `
-    //     <h2>${users[i].name}</h2>
-    //     <p>Aqui terão informações sobre a ${users[i].name}.</p>
-    //   `;
-    // document.getElementById('about').appendChild(uniSection);
+    let uniSection = document.createElement('section');
+    uniSection.id = `#${users[i].name}`;
+    uniSection.classList.add('secao-uni');
+    uniSection.innerHTML = `
+        <h2>${users[i].name}</h2>
+        <p><strong>Usuário:</strong> @${users[i].username}</p>
+        <p><strong>E-mail:</strong> ${users[i].email}</p>
+        <p><strong>Telefone:</strong> ${users[i].phone}</p>
+        <p><strong>Website:</strong> ${users[i].website}</p>
+        <p><strong>Empresa:</strong> ${users[i].company.name}</p>
 
+      `;
+    secaoAbout.appendChild(uniSection);
 }
 const userLayer = L.layerGroup([...userArray]);
 
@@ -51,22 +56,25 @@ const map = L.map('map', {
     layers: [uniLayer, userLayer]
 });
 
-uniLayer.addTo(map); //layer inicial a ser mostrada
-
 map.on('popupopen', function (e) {
     var marker = e.popup._source._popup._content,
         anchor = marker.substring(
             marker.indexOf('"') + 1,
             marker.lastIndexOf('">'));
 
+    let ChosenSection = document.getElementById(anchor);
     document.querySelectorAll('.secao-uni').forEach(e => e.classList.remove('ativo'));
-    document.getElementById(anchor).classList.add('ativo');
+    ChosenSection.classList.add('ativo');
+    setTimeout(() => ChosenSection.scrollIntoView({ block: "start", behavior: "smooth" }), 10);
 });
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 12,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+uniLayer.addTo(map); //layer inicial a ser mostrada
+map.removeLayer(userLayer) // layer a ser escondida inicialmente
 
 const overlayMaps = {
     "Universidades": uniLayer,
